e0a23e90756cabc465d90543f0c7ea4acb575820
ushara <it19152806@my.sliit.lk>
2020-10-03 18:50:16 +0530

    ProjectAdvertise_it19152806-updated-1.1.1-final

--- app/src/main/java/com/example/adverticepro/AdvertiseHome.java
+++ app/src/main/java/com/example/adverticepro/AdvertiseHome.java
@@ -7,14 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class AdvertiseHome extends AppCompatActivity {
 
     Button btnNewHome, btnViewHome, btnMyHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_advertise_home);
 
         btnNewHome = findViewById(R.id.btnNewHome);
         btnViewHome = findViewById(R.id.btnViewHome);
